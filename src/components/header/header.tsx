822cd9461c8ee7b730bda68cdf6b3ca4c1facbb7
mohsen shabanian <mohsen1299@gmail.com>
2019-08-29 12:12:35 +0430

    add rtl

---
+++ src/components/header/header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <div>
+      <h2>logo name</h2>
+      <h3>header span</h3>
+    </div>
+  );
+};
+
+export default Header;
