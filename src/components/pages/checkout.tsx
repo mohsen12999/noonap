3d0075033bc38da8ff0985eef95c5a0385b9f9e7
mohsen shabanian <mohsen1299@gmail.com>
2019-09-16 20:30:26 +0430

    update checkout

--- src/components/pages/checkout.tsx
+++ src/components/pages/checkout.tsx
@@ -7,6 +7,8 @@ import { IShopState, ICartState } from "../../reducers/shop";
 import Container from "@material-ui/core/Container";
 import { makeStyles } from "@material-ui/core/styles";
 
+import { PRODUCT_LIST, IProductSpecific } from "../../actions/shop";
+
 const useStyles = makeStyles({
   mainTitle: {
     fontFamily: "Yekan"
@@ -24,11 +26,21 @@ interface ICheckoutProps {
 const Checkout: React.FC<ICheckoutProps> = (prop: ICheckoutProps) => {
   const classes = useStyles();
 
-  const cartQuantity = prop.cart;
+  const products: IProductSpecific[] = PRODUCT_LIST.map(pCart => {
+    pCart.count =
+      prop.cart === undefined || prop.cart[pCart.id] === undefined
+        ? 0
+        : prop.cart[pCart.id];
+    return pCart;
+  });
+
+  const products2: IProductSpecific[] = products.filter(p => p.count !== 0);
+
+  console.log(products, products2);
 
   return (
     <Container maxWidth="md">
-      <h3 className={classes.mainTitle}>درباره ما</h3>
+      <h3 className={classes.mainTitle}>لیست سفارش ها</h3>
       <p className={classes.mainTitle}>
         کلیه حقوق مادی و معنوی اپلیکیشن نون متعلق به شرکت
         <a
